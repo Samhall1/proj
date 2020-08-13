180be53c6ac2208ace063a9ffd5f6ad96a8f30f2
Neil Munro <neilmunro@gmail.com>
2020-08-13 22:33:14 +0100

    Added loop over basic list

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const Person = require("./Person");
+
+const people = [
+  { name: "Gary", age: 24 },
+  { name: "Gina", age: 18 },
+];
+
+for(i = 0; i < people.length; i++) {
+    const person = new Person(people[i].name, people[i].age);
+    console.log(person.canVote());
+    console.log(person.isActive());
+    person.readBook("a");
+    person.readBook("b");
+    person.readBook("c");
+    console.log(person.isAvidReader());
+    console.log(person.canVote());
+}
