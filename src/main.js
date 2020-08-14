7b337a722c9828751f31275984e0f1d0f11d7621
Alex Coop <alexcoop4@gmail.com>
2020-08-14 11:18:47 +0100

    enhancement/3

--- src/main.js
+++ src/main.js
@@ -14,5 +14,5 @@ for(i = 0; i < people.length; i++) {
     person.readBook("c");
     console.log(person.isAvidReader());
     console.log(person.canVote());
-    console.log(person.toString());
+    console.log(person.toString(people[i].name));
 }
